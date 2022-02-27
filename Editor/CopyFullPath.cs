f395403e14945be28ac101677ed87c8bf1680b04
AC Studio <68843488+ARtronClassicStudio@users.noreply.github.com>
2022-02-27 19:42:58 +0300

    Update CopyFullPath.cs

--- Editor/CopyFullPath.cs
+++ Editor/CopyFullPath.cs
@@ -2,11 +2,11 @@ using System.IO;
 using UnityEditor;
 using UnityEngine;
 
-internal class Copy FullPath : Editor
+internal class CopyFullPath : Editor
 {
-[MenuItem("Assets/Copy Full Path")]
-internal static void Copy()
-{
-GUIUtility.systemCopyBuffer = Path.GetFullPath(AssetDatabase.GetAssetPath(Selection.activeObject));
-}
+    [MenuItem("Assets/Copy Full Path")]
+    internal static void Copy()
+    {
+        GUIUtility.systemCopyBuffer = Path.GetFullPath(AssetDatabase.GetAssetPath(Selection.activeObject));        
+    }
 }
